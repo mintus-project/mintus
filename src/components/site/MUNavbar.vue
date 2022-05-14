22ce77c0f71dd5ecea4b89e4d7307b3a1076a5ec
魏子涵 <muzhenweizh@gmail.com>
2022-05-14 16:20:04 +0800

    创建组件：MUFooter，MUSiteProfile

--- src/components/site/MUNavbar.vue
+++ src/components/site/MUNavbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full bg-transparent px-12 py-3 flex justify-between items-center"
+    class="w-full h-20 bg-transparent px-12 py-3 flex justify-between items-center"
   >
     <span class="text-3xl font-black text-[#5D5FEF] cursor-pointer" @click="$router.push('/')">MintUs</span>
     <MUButton>Connect Wallet</MUButton>
