a194039a710a6a6f5f78d6003f48ef45358ee2b4
马英健 <704886043@qq.com>
2022-05-22 17:35:54 +0800

    MUWalletAddr badge样式修改，personalInfo使用MUWalletAddr

--- src/pages/user/profile/components/PersonalInfo.vue
+++ src/pages/user/profile/components/PersonalInfo.vue
@@ -6,7 +6,11 @@
       </div>
     </div>
     <span class="font-bold text-3xl">{{ $route.params.id }}</span>
-    <span>wallet address</span>
+    <!-- <span>wallet address</span> -->
+    <div class="flex border border-gray-200 rounded-full py-2 px-4 gap-2">
+      <MUCoin type="eth" />
+      <MUWalletAddr :addr="store.walletInfo.address" />
+    </div>
     <div class="flex gap-4">
       <MUIconButton @click="$router.push('setting')">
         <template #icon
@@ -23,6 +27,11 @@
   import MUIconButton from '../../../../components/common/MUIconButton.vue'
   import MUButton from '../../../../components/common/MUButton.vue'
   import { Icon } from '@iconify/vue'
+  import MUWalletAddr from '@/components/common/MUWalletAddr.vue'
+  import { useStore } from '@/store'
+  import MUCoin from '@/components/common/MUCoin.vue'
+
+  const store = useStore()
 </script>
 
 <style lang="scss" scoped></style>
